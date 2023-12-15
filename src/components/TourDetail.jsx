93d5b15005baaab58bcd41c6c2e5d6706f40754a
Hamza59272 <hs623965@gmail.com>
2023-12-15 23:31:53 +0500

    currency option adeed in Tour details

--- src/components/TourDetail.jsx
+++ src/components/TourDetail.jsx
@@ -18,13 +18,16 @@ const PackageDetails = () => {
   const [packagesData, setPackagesData] = useState([]);
   const [packageObject, setPackageObject] = useState();
   const [openModal, setOpenModal] = useState(false);
-
+  const [currencyOptions, setCurrencyOptions] = useState([])
+  const [fromCurrency, setFromCurrency] = useState()
+  const [toCurrency, setToCurrency] = useState()
+  const [exchangeRate, setExchangeRate] = useState()
+  const [CurrentCurrency, setCurrentCurrency ] = useState()
   useEffect(() => {
     const URL = "https://backend.azeemtourism.com/api/tours/get";
     axios
       .get(URL)
       .then((response) => {
-        console.log(response.data);
         const filtered = response.data.filter(
           (packages) => packages.active === true,
         );
@@ -35,11 +38,31 @@ const PackageDetails = () => {
       .catch((error) => {
         console.log(error.message);
       });
+
+
+      axios.get(`http://api.exchangeratesapi.io/v1/latest?access_key=${import.meta.env.VITE_REACT_APP_EXCHANGE_RATE_API_KEY}`)
+      .then(response => {
+        const baseCurrency = "USD"; 
+        const targetCurrency = "AED";
+        setCurrencyOptions([baseCurrency, ...Object.keys(response.data.rates)]);
+        setFromCurrency(baseCurrency);
+        setToCurrency(targetCurrency);
+        setCurrentCurrency(response.data.rates[baseCurrency])
+        setExchangeRate(response.data.rates[targetCurrency])
+      })
+
   }, []);
 
   useEffect(() => {
     window.scrollTo(0, 0);
   }, []);
+
+  const handleCurrencyToggle = () => {
+    setFromCurrency(toCurrency)
+    setToCurrency(fromCurrency)
+   
+  };
+  
 
   const validationSchema = Yup.object({
     full_name: Yup.string()
@@ -163,11 +186,21 @@ const PackageDetails = () => {
                 <div className="flex ">
                   <p className="flex text-xl font-inter font-semibold mt-1 gap-x-2">
                     <IoPricetagsOutline className="mt-1" />
-                    Price: $
+                    Price: {fromCurrency === "USD" ? "$" : "AED: "}
                   </p>
                   <span className="font-inter font-bold text-xl mt-1">
-                    {packageObject.price}
+                    {fromCurrency === "USD"
+                      ? packageObject.price
+                      : Math.round ((exchangeRate / CurrentCurrency) * packageObject.price,9)}
                   </span>
+                  <Button
+                    className="ml-5 shadow-sm bg-black text-white hover:bg-white hover:text-black transition-colors duration-100 text-xs md:text-sm font-medium text-center rounded-lg md:p-3"
+                    onClick={() => handleCurrencyToggle(fromCurrency)}
+                  >
+                    Change Price to {toCurrency}
+                  </Button>
+
+
                 </div>
               </div>
               <div className="w-full lg:w-2/5 ">
